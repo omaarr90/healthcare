0a1e10457ca9e9f63b454dc2784948084495777c
Omar Alshammary <omar.alshammary@hotmail.com>
2016-12-26 23:46:57 +0300

    fix sending email

--- Sources/App/Controllers/MailClient.swift
+++ Sources/App/Controllers/MailClient.swift
@@ -6,6 +6,7 @@
 //
 //
 
+import Vapor
 import SMTP
 import TLS
 import Transport
@@ -19,44 +20,25 @@ func setupClient() {
 
 class MailClient
 {
-    
-    static func sendTestEmail() throws {
-        let credentials = SMTPCredentials(
-            user: "healthcareapp3@gmail.com",
-            pass: "Wadha123!@"
-        )
-        let from = EmailAddress(name: "Test",
-                                address: "healthcareapp3@gmail.com")
-        let to = "omar.alshammary@hotmail.com"
-        let email: Email = Email(from: from,
-                                 to: to,
-                                 subject: "Vapor SMTP - Simple",
-                                 body: "Hello from Vapor SMTP 👋")
         
-        let client = try SMTPClient<TCPClientStream>.makeGmailClient()
-        try client.send(email, using: credentials)
-
-
-    }
-    
     static func sendAppointmentConfirmation(to email: String, token: String) throws
     {
         
         let confirmURL = "http://172.20.10.13:8088/confirmAppintment?token=\(token)"
         
         let credentials = SMTPCredentials(
-            user: "healthcareapp3@gmail.com",
-            pass: "Wadha123!@"
+            user: Env.get("MAIL_USERNAME")!,
+            pass: Env.get("MAIL_PASSWORD")!
         )
-        let from = EmailAddress(name: "Test",
+        let from = EmailAddress(name: "Patient Reservation System",
                                 address: "healthcareapp3@gmail.com")
         let to = email
-        let email: Email = Email(from: from,
+        let email: SMTP.Email = Email(from: from,
                                  to: to,
                                  subject: "تأكيد حجز الموعد",
                                  body: "من فضلك قم باستخدام الرابط التالي لتأكيد الحجز \(confirmURL)")
         
-        let client = try SMTPClient<TCPClientStream>.makeGmailClient()
+        let client = try SMTPClient<TCPClientStream>.makeSendGridClient()
         try client.send(email, using: credentials)
     }
         
